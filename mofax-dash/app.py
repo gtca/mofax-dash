d99a0af46ec12e1ac9c63b1e724ac46120db1dda
Danila Bredikhin <danila.bredikhin@embl.de>
2020-04-29 03:25:54 +0200

    Update app to include view, group, and factor subsetting

--- mofax-dash/app.py
+++ mofax-dash/app.py
@@ -14,6 +14,8 @@ from contextlib import ExitStack
 from functools import partial
 import base64
 
+import numpy as np
+
 import mofax as mfx
 
 
@@ -34,6 +36,10 @@ if __name__ == '__main__':
         
         model = mfx.mofa_model(filename)
         model_filename = filename
+
+        # Number of factors explaining more than 1% of variance 
+        # to be reused later for better default view
+        signif_k = 0
 
         # # Use ExitStack to defer model.close()
         # with ExitStack() as stack:
@@ -61,10 +67,10 @@ def make_card(main_value, main_desc, sub_value, sub_desc):
             ])
 
 
-def make_card_children(main_value, main_desc, sub_value, sub_desc):
+def make_card_children(main_value, main_desc, prep, sub_value, sub_desc, more=""):
     return [
                 html.Div(className="card-maintext", children=f"{main_value} {main_desc}"),
-                html.Div(className="card-subtext", children=f"in {sub_value} {sub_desc}{'s' if sub_value > 1 else ''}")
+                html.Div(className="card-subtext", children=f"{prep} {sub_value} {sub_desc}{'s' if sub_value > 1 else ''} {more}")
            ]
 
 
@@ -74,20 +80,22 @@ def update_n(model):
     if model is not None:
         dim_n = model.shape[0]
         dim_g = len(model.groups)
-        return make_card_children(dim_n, "cells", dim_g, "group")
+        return make_card_children(dim_n, "cells", "in", dim_g, "group")
 
 
 def update_d(model):
     if model is not None:
         dim_d = model.shape[1]
         dim_m = len(model.views)
-        return make_card_children(dim_d, "features", dim_m, "view")
+        return make_card_children(dim_d, "features", "in", dim_m, "view")
 
 
 def update_k(model):
     if model is not None:
+        global signif_k
         dim_k = model.nfactors
-        return make_card_children(dim_k, "factors", dim_k, "factor")
+        signif_k = np.sum(model.get_r2().groupby("Factor").agg({"R2": "max"}).R2 > 0.01)
+        return make_card_children(dim_k, "factors", "where", signif_k, "factor", more="explain >1% variance")
 
 
 def update_factors(model):
@@ -127,9 +135,10 @@ def update_factors(model):
 
 
 
-def update_factors_violin(model):
+def update_factors_violin(factors):
+    if factors is not None and (factors == -1 or -1 in factors): factors = None
     if model is not None:
-        df = model.get_factors(factors=None, df=True)\
+        df = model.get_factors(factors=factors, df=True)\
                   .rename_axis("Sample")\
                   .reset_index()\
                   .melt(var_name="Factor", value_name="Value", id_vars=["Sample"])
@@ -151,16 +160,32 @@ def update_factors_violin(model):
                 'yaxis': {
                     'title': "Factor value",
                 },
+                'margin': {
+                    'l': 100,
+                    'r': 20,
+                    'b': 50,
+                    't': 20,
+                }
             }
+        }, style={
+            'width': '100%',
+            'height': '100%',
+            'margin': 0,
+            'padding': 0,
         })
         return fig
 
 
 # TODO: divide into update R2 heatmap and update selectors
 # see https://github.com/balajiciet/daypart/blob/master/daypart.py
-def update_r2(model):
+def update_r2(groups, views, factors):
+    # Map -1 to None
+    # This is due to inability to pass None as value
+    if views is not None and (views == -1 or -1 in views): views = None
+    if groups is not None and (groups == -1 or -1 in groups): groups = None
+    if factors is not None and (factors == -1 or -1 in factors): factors = None
     if model is not None:
-        df = model.get_r2()
+        df = model.get_r2(groups=groups, views=views, factors=factors)
         fig = dcc.Graph(id="r2-plot", figure={
             'data': [
                 go.Heatmap(
@@ -170,18 +195,29 @@ def update_r2(model):
                     colorscale='Purples',
                 )
             ],
-            'layout': go.Layout(
-                clickmode= 'event+select',
-                xaxis= {
+            'layout': {
+                'clickmode': 'event+select',
+                'xaxis': {
                     'title': "Group",
                 },
-                yaxis= {
-                    'title': "Factor",
+                'yaxis': {
+                    'title': "",
                 },
-            )
+                'margin': {
+                    'l': 100,
+                    'r': 20,
+                    'b': 50,
+                    't': 20,
+                }
+            }
+        }, style={
+            'width': '100%',
+            'height': '100%',
+            'margin': 0,
+            'padding': 0,
         })
 
-        sel  = html.Div([
+        sel = html.Div([
             html.Div([
                 html.H5('Select data subsetting'),
                 dcc.Dropdown(
@@ -202,7 +238,7 @@ def update_r2(model):
                 ],
                 style={'width': '30%', 'float': 'right', 'display': 'inline-block'}),
             ])
-        return [sel, fig]
+        return [fig]
 
 
 
@@ -214,16 +250,50 @@ card_dim_d = html.Div(id="card-dim-d", className="card card-small", children=upd
 card_dim_k = html.Div(id="card-dim-k", className="card card-small", children=update_k(model))
 
 card_settings = html.Div(id="card-settings", className="card", children=[
+    html.Div(id="settings-subset", className="settings-row", children=[
+        html.Div(children=[
+            html.Span('Views'),
+            dcc.Dropdown(
+                id="views-selection",
+                options=[{'label': "All", 'value': -1}] + [{'label': m, 'value': m} for m in model.views],
+                value=-1,
+                multi=True,
+                ),
+            ],
+        ),
+
+        html.Div(children=[
+            html.Span('Groups'),
+            dcc.Dropdown(
+                id="groups-selection",
+                options=[{'label': "All", 'value': -1}] + [{'label': g, 'value': g} for g in model.groups],
+                value=-1,
+                multi=True,
+                ),
+            ],
+        ),
+    ]),
     
-    
+    html.Div(className="settings-row", children=[ 
+        html.Div(children=[
+            html.Span('Factors'),
+            dcc.Dropdown(
+                id="factors-selection",
+                options=[{'label': "All", 'value': -1}] + [{'label': k, 'value': k} for k in [f"Factor{i+1}" for i in range(model.nfactors)]],
+                value=-1 if signif_k == 0 else [f"Factor{i+1}" for i in range(signif_k)],
+                multi=True,
+                ),
+            ],
+        ),
+    ]),
 ])
 
-card_r2 = html.Div(id="card-r2", className="card", children=update_r2(model))
+card_r2 = html.Div(id="card-r2", className="card", children=update_r2(None, None, None))
 
 
 card_factors = html.Div(id="card-factors", className="card", children=[update_factors(model)])
 
-card_factors_violin = html.Div(id="card-factors-violin", className="card", children=[update_factors_violin(model)])
+card_factors_violin = html.Div(id="card-factors-violin", className="card", children=[update_factors_violin(None)])
 
 cards = html.Div(id="cardboard", children=[card_dim_n, card_dim_d, card_dim_k, card_settings, card_r2, card_factors, card_factors_violin])
 
@@ -281,6 +351,18 @@ app.layout = html.Div(id='content', children=[
 #         fp.write(base64.decodebytes(data))
 
 
+
+
+# Callbacks
+
+app.callback(Output('card-r2', 'children'),
+    [Input('groups-selection', 'value'),
+     Input('views-selection', 'value'),
+     Input('factors-selection', 'value')])(update_r2)
+
+
+app.callback(Output('card-factors-violin', 'children'),
+    [Input('factors-selection', 'value')])(update_factors_violin)
 
 
 if __name__ == "__main__":
